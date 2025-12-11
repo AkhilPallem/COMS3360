df0ffdc260fadde57abc2ee57324a9bb36059a25
Akhil Pallem <pallemakhil@icloud.com>
2025-12-11 11:17:20 -0600

    added texture class to work on perlin noise and added into my material types

--- main.cpp
+++ main.cpp
@@ -109,7 +109,9 @@ enum MaterialType {
     DIFFUSE,
     METAL,
     DIELECTRIC,
-    EMISSIVE
+    EMISSIVE,
+    PERLIN_NOISE,
+    PERLIN_MARBLE 
 };
 
 struct Material {
@@ -253,6 +255,101 @@ public:
         
         std::cout << "Loaded texture: " << filename << " (" << tex.width << "x" << tex.height << ")" << std::endl;
         return tex;
+    }
+};
+
+class PerlinNoise {
+private:
+    static const int pointCount = 256;
+    Vec3* randVec;
+    int* permX;
+    int* permY;
+    int* permZ;
+    
+    static int* perlinGeneratePerm() {
+        int* p = new int[pointCount];
+        for (int i = 0; i < pointCount; i++)
+            p[i] = i;
+        
+        for (int i = pointCount - 1; i > 0; i--) {
+            int target = int(randomDouble(0, i + 1));
+            std::swap(p[i], p[target]);
+        }
+        return p;
+    }
+    
+    static double perlinInterp(Vec3 c[2][2][2], double u, double v, double w) {
+        double uu = u * u * (3 - 2 * u);
+        double vv = v * v * (3 - 2 * v);
+        double ww = w * w * (3 - 2 * w);
+        double accum = 0.0;
+        
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                for (int k = 0; k < 2; k++) {
+                    Vec3 weightV(u - i, v - j, w - k);
+                    accum += (i * uu + (1 - i) * (1 - uu)) *
+                            (j * vv + (1 - j) * (1 - vv)) *
+                            (k * ww + (1 - k) * (1 - ww)) *
+                            c[i][j][k].dot(weightV);
+                }
+        return accum;
+    }
+    
+public:
+    PerlinNoise() {
+        randVec = new Vec3[pointCount];
+        for (int i = 0; i < pointCount; i++) {
+            randVec[i] = Vec3::random(-1, 1).normalize();
+        }
+        
+        permX = perlinGeneratePerm();
+        permY = perlinGeneratePerm();
+        permZ = perlinGeneratePerm();
+    }
+    
+    ~PerlinNoise() {
+        delete[] randVec;
+        delete[] permX;
+        delete[] permY;
+        delete[] permZ;
+    }
+    
+    double noise(const Vec3& p) const {
+        double u = p.x - floor(p.x);
+        double v = p.y - floor(p.y);
+        double w = p.z - floor(p.z);
+        
+        int i = int(floor(p.x));
+        int j = int(floor(p.y));
+        int k = int(floor(p.z));
+        
+        Vec3 c[2][2][2];
+        
+        for (int di = 0; di < 2; di++)
+            for (int dj = 0; dj < 2; dj++)
+                for (int dk = 0; dk < 2; dk++)
+                    c[di][dj][dk] = randVec[
+                        permX[(i + di) & 255] ^
+                        permY[(j + dj) & 255] ^
+                        permZ[(k + dk) & 255]
+                    ];
+        
+        return perlinInterp(c, u, v, w);
+    }
+    
+    double turbulence(const Vec3& p, int depth = 7) const {
+        double accum = 0.0;
+        Vec3 tempP = p;
+        double weight = 1.0;
+        
+        for (int i = 0; i < depth; i++) {
+            accum += weight * noise(tempP);
+            weight *= 0.5;
+            tempP = tempP * 2.0;
+        }
+        
+        return fabs(accum);
     }
 };
 
